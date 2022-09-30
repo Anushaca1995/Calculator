abb525bc6b29fc0a2bfc87e2f7146e65c6652d45
Anusha Antony <anushaca1995@gmail.com>
2022-09-30 15:04:15 +0100

    Added key symbols function for operators onclick method

--- script.js
+++ script.js
@@ -9,6 +9,14 @@ const calcObj = {
         this.screenMemory += ''+num;
         console.log('Value of screen memory:', this.screenMemory);
     },
-    
+    keySymbols: function (symbol){
+        document.querySelector('.calcans').innerHTML +=symbol;
+        document.querySelector('.calcans').value +=symbol;
+        this.memory.push(this.screenMemory);
+        this.screenMemory='';
+        this.operator=symbol;
+        console.log('screen memory now:', this.screenMemory);
+        console.log("memory=",this.memory);
+    }
 
 }
